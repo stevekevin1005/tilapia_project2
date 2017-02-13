d3dd6d14c3c4fc01d829c6396439b3b9e5367b3a
stevekevin1005 <qqwwee2006@gmail.com>
2017-02-13 22:32:05 +0800

    add sort

--- api/services/Tilapia2Service.js
+++ api/services/Tilapia2Service.js
@@ -77,6 +77,17 @@ module.exports = {
 				}
 			}
 
+			result = result.sort(function (a, b) {
+			  if (a.id > b.id) {
+			    return 1;
+			  }
+			  if (a.id < b.id) {
+			    return -1;
+			  }
+			  // a must be equal to b
+			  return 0;
+			})
+
 			return result;
 		}catch(e){
 			console.error('e->', e);
